f34308118d9404e5f5681639d92bd0df39f2932f
Kayuemkhan <abdulkayuem007@gmail.com>
2021-01-07 23:52:09 +0600

    1.1

--- app/src/main/java/code/fortomorrow/animallover/LaunchActivity.java
+++ app/src/main/java/code/fortomorrow/animallover/LaunchActivity.java
@@ -3,7 +3,9 @@ package code.fortomorrow.animallover;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -15,13 +17,37 @@ public class LaunchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
         ButterKnife.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.red, this.getTheme()));
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.red));
+        }
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(2*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
     }
-    @OnClick(R.id.registerfromlaunch)
-    public void registerfromlaunch(){
-        startActivity(new Intent(LaunchActivity.this,SignupActivity.class));
-    }
-    @OnClick(R.id.loginfromlaunch)
-    public void loginfromlaunch(){
-        startActivity(new Intent(LaunchActivity.this,LoginActivity.class));
-    }
+//    @OnClick(R.id.registerfromlaunch)
+//    public void registerfromlaunch(){
+//        startActivity(new Intent(LaunchActivity.this,SignupActivity.class));
+//    }
+//    @OnClick(R.id.loginfromlaunch)
+//    public void loginfromlaunch(){
+//        startActivity(new Intent(LaunchActivity.this,LoginActivity.class));
+//    }
+
 }
