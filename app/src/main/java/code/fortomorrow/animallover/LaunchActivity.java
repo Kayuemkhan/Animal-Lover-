746e4ba005bf88216ef71c3e209393bfc446a97d
Kayuemkhan <abdulkayuem007@gmail.com>
2021-01-13 23:08:12 +0600

    setting up viewpager in welcome page

--- app/src/main/java/code/fortomorrow/animallover/LaunchActivity.java
+++ app/src/main/java/code/fortomorrow/animallover/LaunchActivity.java
@@ -1,52 +1,200 @@
 package code.fortomorrow.animallover;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LaunchActivity extends AppCompatActivity {
 
+    private static final int MAX_STEP = 4;
+
+    private ViewPager viewPager;
+    private Button btnNext;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private String about_title_array[] = {
+            "Ready to Travel",
+            "Pick the Ticket",
+            "Flight to Destination",
+            "Enjoy Holiday"
+    };
+    private String about_description_array[] = {
+            "Choose your destination, plan Your trip. Pick the best place for Your holiday",
+            "Select the day, pick Your ticket. We give you the best prices. We guarantee!",
+            "Safe and Comfort flight is our priority. Professional crew and services.",
+            "Enjoy your holiday, Don't forget to feel the moment and take a photo!",
+    };
+    private int about_images_array[] = {
+            R.drawable.catback,
+            R.drawable.animal_lover_background,
+            R.drawable.splash_screen,
+            R.drawable.deginupper
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
-        ButterKnife.bind(this);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().setStatusBarColor(getResources().getColor(R.color.red, this.getTheme()));
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(getResources().getColor(R.color.red));
-        }
-        Thread background = new Thread() {
-            public void run() {
-                try {
-                    // Thread will sleep for 5 seconds
-                    sleep(2*1000);
 
-                    // After 5 seconds redirect to another intent
-                    Intent i=new Intent(getApplicationContext(),LoginActivity.class);
-                    startActivity(i);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        btnNext = (Button) findViewById(R.id.btn_next);
 
-                    finish();
-                } catch (Exception e) {
+        // adding bottom dots
+        bottomProgressDots(0);
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        viewPager.setPageMargin(-60);
+        viewPager.setOffscreenPageLimit(4);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (viewPager.getCurrentItem() == about_title_array.length - 1) {
+                    btnNext.setText("Get Started");
+                } else {
+                    btnNext.setText("Next");
                 }
             }
-        };
-        // start thread
-        background.start();
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = viewPager.getCurrentItem() + 1;
+                if (current < MAX_STEP) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
+                } else {
+                    finish();
+                }
+            }
+        });
+
+//        Tools.setSystemBarColor(this, R.color.grey_10);
+//        Tools.setSystemBarLight(this);
     }
-//    @OnClick(R.id.registerfromlaunch)
-//    public void registerfromlaunch(){
-//        startActivity(new Intent(LaunchActivity.this,SignupActivity.class));
-//    }
-//    @OnClick(R.id.loginfromlaunch)
-//    public void loginfromlaunch(){
-//        startActivity(new Intent(LaunchActivity.this,LoginActivity.class));
-//    }
+
+
+    private void bottomProgressDots(int current_index) {
+        LinearLayout dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        ImageView[] dots = new ImageView[MAX_STEP];
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new ImageView(this);
+            int width_height = 15;
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(width_height, width_height));
+            params.setMargins(10, 10, 10, 10);
+            dots[i].setLayoutParams(params);
+            dots[i].setImageResource(R.drawable.ic_baseline_vpn_key_24);
+            dots[i].setColorFilter(getResources().getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[current_index].setImageResource(R.drawable.ic_baseline_email_24);
+            dots[current_index].setColorFilter(getResources().getColor(R.color.light_green_600), PorterDuff.Mode.SRC_IN);
+        }
+    }
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(final int position) {
+            bottomProgressDots(position);
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    /**
+     * View pager adapter
+     */
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(R.layout.item_card_wizard, container, false);
+            ((TextView) view.findViewById(R.id.title)).setText(about_title_array[position]);
+            ((TextView) view.findViewById(R.id.description)).setText(about_description_array[position]);
+            ((ImageView) view.findViewById(R.id.image)).setImageResource(about_images_array[position]);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return about_title_array.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
 
 }
