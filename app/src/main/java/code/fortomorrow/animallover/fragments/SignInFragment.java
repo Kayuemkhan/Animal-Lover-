74dfbe0febe4c19e1142840b008f5e51ca6baf94
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-18 08:31:00 +0600

    firebase authentication

--- app/src/main/java/code/fortomorrow/animallover/fragments/SignInFragment.java
+++ app/src/main/java/code/fortomorrow/animallover/fragments/SignInFragment.java
@@ -1,18 +1,41 @@
 package code.fortomorrow.animallover.fragments;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import code.fortomorrow.animallover.HomeActivity;
+import code.fortomorrow.animallover.MainActivity;
+import code.fortomorrow.animallover.ModelClass.Users;
 import code.fortomorrow.animallover.R;
 
 
 public class SignInFragment extends Fragment {
-
+    private EditText InputNumber, InputPassword;
+    private TextView LoginButton;
+    ProgressDialog loadingBar1,loadingBar2;
+    private TextView signupbtn;
+    private String parentDbName ="Users";
+    private CheckBox chkBoxRememberMe;
     public SignInFragment() {
 
     }
@@ -27,7 +50,79 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        LoginButton = view.findViewById(R.id.login_btn);
+        InputNumber = view.findViewById(R.id.login_phone_number_input);
+        InputPassword = view.findViewById(R.id.login_password_input);
+        loadingBar1 = new ProgressDialog(getActivity());
+        chkBoxRememberMe = view.findViewById(R.id.remember_me_chkb);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
 
         return view;
     }
+
+    private void loginUser() {
+        String phone = InputNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if(TextUtils.isEmpty(phone)){
+            InputNumber.setError("Phone Field can't be Blank");
+            return;
+        }
+        else if(TextUtils.isEmpty(password)){
+            InputPassword.setError("Password Field can't be Blank");
+            return;
+        }
+        else {
+            loadingBar1.setTitle("Login Account");
+            loadingBar1.setMessage("Please Wait, While we are checking the credentials");
+            loadingBar1.setCanceledOnTouchOutside(false);
+            loadingBar1.show();
+            AllowAccssAccount(phone,password);
+        }
+    }
+    private void AllowAccssAccount(final String phone, final String password) {
+
+        final DatabaseReference Rootref;
+        Rootref = FirebaseDatabase.getInstance().getReference();
+        Rootref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(parentDbName).child(phone).exists()){
+                    Users usersData = dataSnapshot.child(parentDbName).child(phone).getValue(Users.class);
+
+                    if(usersData.getPhone().equals(phone)){
+                        if(usersData.getPassword().equals(password)){
+                            if (parentDbName.equals("Users")) {
+                                loadingBar1.dismiss();
+                                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                                startActivity(intent);
+                                getActivity().finish();
+                            }
+
+                        }
+                        else {
+                            loadingBar1.dismiss();
+                            Toast.makeText(getActivity(),"password is not Correct",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }
+                else {
+                    Toast.makeText(getActivity(),"Account with this "+ phone+" number do not exists ",Toast.LENGTH_SHORT).show();
+                    loadingBar1.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/code/fortomorrow/animallover/fragments/SignUpFragment.java
+++ app/src/main/java/code/fortomorrow/animallover/fragments/SignUpFragment.java
@@ -1,17 +1,41 @@
 package code.fortomorrow.animallover.fragments;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+import code.fortomorrow.animallover.HomeActivity;
+import code.fortomorrow.animallover.MainActivity;
 import code.fortomorrow.animallover.R;
 
 public class SignUpFragment extends Fragment {
 
+    private TextView createAccountButton;
+    private EditText InputName, InputPhoneNumber, InputPassword;
+    private ProgressDialog loadingBar;
+    private TextView login_back;
 
     public SignUpFragment() {
     }
@@ -26,7 +50,89 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-       View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        createAccountButton = view.findViewById(R.id.register_btn);
+        InputName = view.findViewById(R.id.register_username_input);
+        InputPhoneNumber = view.findViewById(R.id.register_phone_number_input);
+        InputPassword = view.findViewById(R.id.register_password_input);
+        loadingBar = new ProgressDialog(getActivity());
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount();
+            }
+        });
         return view;
+    }
+
+    private void createAccount() {
+        String name = InputName.getText().toString();
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(name)) {
+            InputName.setError("Name Field can't be Blank");
+            return;
+        } else if (TextUtils.isEmpty(phone)) {
+            InputPhoneNumber.setError("Phone Field can't be Blank");
+            return;
+        } else if (TextUtils.isEmpty(password)) {
+            InputPassword.setError("Password Field can't be Blank");
+            return;
+        } else {
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please Wait, While we are checking the credentials");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            validatephoneNumber(name, phone, password);
+        }
+    }
+
+    private void validatephoneNumber(final String name, final String phone, final String password) {
+        final DatabaseReference Rootref;
+        Rootref = FirebaseDatabase.getInstance().getReference();
+
+        Rootref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!(dataSnapshot.child("Users").child(phone).exists())) {
+                    HashMap<String, Object> userDataMap = new HashMap<>();
+                    userDataMap.put("phone", phone);
+                    userDataMap.put("password", password);
+                    userDataMap.put("name", name);
+
+                    Rootref.child("Users").child(phone).updateChildren(userDataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getActivity(), "Congratulations , Your account is created", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+                                        Intent intent = new Intent(getActivity(), HomeActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(getActivity(), "NetWork error! Please, try again", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(getActivity(), "This " + phone + "Already exists", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(getActivity(), "Please try again using another Phone Number", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getActivity(), HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
