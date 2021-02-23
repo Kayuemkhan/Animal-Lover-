99aa59f7591370cf2b1aa4d89018f52052102f64
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-23 23:30:06 +0600

    23-02-2021

--- app/src/main/java/code/fortomorrow/animallover/HealthActivity.java
+++ app/src/main/java/code/fortomorrow/animallover/HealthActivity.java
@@ -6,9 +6,11 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class HealthActivity extends AppCompatActivity {
     private CardView medicinecard,diseasecard,healthcard;
+    private ImageView backfrommyhealth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +22,17 @@ public class HealthActivity extends AppCompatActivity {
                 startActivity(new Intent(HealthActivity.this, MedicinePetActivity.class));
             }
         });
+        backfrommyhealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HealthActivity.this,HomeActivity.class));
+            }
+        });
     }
 
     private void init() {
         medicinecard = findViewById(R.id.medicinecard);
+        backfrommyhealth = findViewById(R.id.backfrommyhealth);
         diseasecard = findViewById(R.id.diseasecard);
         healthcard = findViewById(R.id.healthcard);
     }
--- app/src/main/java/code/fortomorrow/animallover/MyprofileActivity.java
+++ app/src/main/java/code/fortomorrow/animallover/MyprofileActivity.java
@@ -2,13 +2,24 @@ package code.fortomorrow.animallover;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MyprofileActivity extends AppCompatActivity {
-
+    private ImageView backfrommyprofile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
+        backfrommyprofile = findViewById(R.id.backfrommyprofile);
+        backfrommyprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyprofileActivity.this,HomeActivity.class));
+                finish();
+            }
+        });
     }
 }
