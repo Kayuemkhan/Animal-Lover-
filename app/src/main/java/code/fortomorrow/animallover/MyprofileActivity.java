869a6aeb9831f8213ad94881bb2525b3b83067f0
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-28 23:58:27 +0600

    28-02-2021

--- app/src/main/java/code/fortomorrow/animallover/MyprofileActivity.java
+++ app/src/main/java/code/fortomorrow/animallover/MyprofileActivity.java
@@ -11,14 +11,32 @@ import android.widget.TextView;
 import code.fortomorrow.animallover.utils.SharedPref;
 
 public class MyprofileActivity extends AppCompatActivity {
-    private ImageView backfrommyprofile;
-    private TextView username;
+    private ImageView backfrommyprofile,petprofilepic;
+    private TextView username,mypet,mypetcolor,mypetgender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
+       init();
         SharedPref.init(MyprofileActivity.this);
-        username = findViewById(R.id.username);
+        if (SharedPref.read("Animal", "").contains("dog")) {
+            petprofilepic.setImageResource(R.drawable.dog);
+        }
+        if (SharedPref.read("Animal", "").contains("cat")) {
+            petprofilepic.setImageResource(R.drawable.cat);
+        }
+        if (SharedPref.read("Animal", "").contains("rabbit")) {
+            petprofilepic.setImageResource(R.drawable.rabbit);
+        }
+        if (SharedPref.read("Animal", "").contains("fish")) {
+            petprofilepic.setImageResource(R.drawable.fish);
+        }
+        if (SharedPref.read("Animal", "").contains("bird")) {
+            petprofilepic.setImageResource(R.drawable.bird);
+        }
+        username.setText(String.valueOf(SharedPref.read("Phone","")));
+        mypet.setText(String.valueOf(SharedPref.read("Animal", "")));
+        username.setText(String.valueOf(SharedPref.read("Phone","")));
         username.setText(String.valueOf(SharedPref.read("Phone","")));
         backfrommyprofile = findViewById(R.id.backfrommyprofile);
         backfrommyprofile.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +46,13 @@ public class MyprofileActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void init() {
+        username = findViewById(R.id.username);
+        mypet = findViewById(R.id.mypet);
+        mypetcolor = findViewById(R.id.mypetcolor);
+        mypetgender = findViewById(R.id.mypetgender);
+        petprofilepic = findViewById(R.id.petprofilepic);
     }
 }
