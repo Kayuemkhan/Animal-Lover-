7734da38882b05aa49ee310b84e5bc3c25a183e0
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-28 23:27:42 +0600

    my pets logo and my profile page updated

--- app/src/main/java/code/fortomorrow/animallover/MyprofileActivity.java
+++ app/src/main/java/code/fortomorrow/animallover/MyprofileActivity.java
@@ -6,13 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import code.fortomorrow.animallover.utils.SharedPref;
 
 public class MyprofileActivity extends AppCompatActivity {
     private ImageView backfrommyprofile;
+    private TextView username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
+        SharedPref.init(MyprofileActivity.this);
+        username = findViewById(R.id.username);
+        username.setText(String.valueOf(SharedPref.read("Phone","")));
         backfrommyprofile = findViewById(R.id.backfrommyprofile);
         backfrommyprofile.setOnClickListener(new View.OnClickListener() {
             @Override
