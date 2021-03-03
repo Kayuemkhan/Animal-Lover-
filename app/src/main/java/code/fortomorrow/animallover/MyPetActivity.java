8cfbd726f829768b1b27a4c9db2b1d41c41e85ab
Kayuemkhan <abdulkayuem007@gmail.com>
2021-03-04 00:27:21 +0600

    vetenary doctor updates

--- app/src/main/java/code/fortomorrow/animallover/MyPetActivity.java
+++ app/src/main/java/code/fortomorrow/animallover/MyPetActivity.java
@@ -25,7 +25,7 @@ public class MyPetActivity extends AppCompatActivity {
     private EditText petColorET;
     private EditText notesPet;
     private TextView savePetDetails;
-    private String animal,petBirthday,petColor;
+    private String animal = "", petBirthday, petColor;
     private EditText yourplaceET;
     String petnamelist[] = {"Rabbit", "Cat", "Dog", "Bird", "Fish"};
     String gen[] = {"Male", "Female"};
@@ -64,7 +64,8 @@ public class MyPetActivity extends AppCompatActivity {
         savePetDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MyPetActivity.this,HomeActivity.class));
+
+                startActivity(new Intent(MyPetActivity.this, HomeActivity.class));
                 finish();
             }
         });
@@ -76,8 +77,8 @@ public class MyPetActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                petBirthday= s.toString();
-                SharedPref.write("PetBirthDay",petBirthday);
+                petBirthday = s.toString();
+                SharedPref.write("PetBirthDay", petBirthday);
             }
 
             @Override
@@ -93,8 +94,8 @@ public class MyPetActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                petColor= s.toString();
-                SharedPref.write("petColor",petBirthday);
+                petColor = s.toString();
+                SharedPref.write("petColor", petBirthday);
             }
 
             @Override
@@ -124,20 +125,22 @@ public class MyPetActivity extends AppCompatActivity {
         mypetSpinnerSet.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (animal.equals("cat")) {
-                    mypetSpinnerSet.setSelection(1);
-                }
-                if (animal.equals("dog")) {
-                    mypetSpinnerSet.setSelection(1);
-                }
-                if (animal.equals("rabbit")) {
-                    mypetSpinnerSet.setSelection(0);
-                }
-                if (animal.equals("bird")) {
-                    mypetSpinnerSet.setSelection(3);
-                }
-                if (animal.equals("fish")) {
-                    mypetSpinnerSet.setSelection(4);
+                if (!animal.isEmpty()) {
+                    if (animal.equals("cat") && !animal.isEmpty()) {
+                        mypetSpinnerSet.setSelection(1);
+                    }
+                    if (animal.equals("dog") && !animal.isEmpty()) {
+                        mypetSpinnerSet.setSelection(1);
+                    }
+                    if (animal.equals("rabbit") && !animal.isEmpty()) {
+                        mypetSpinnerSet.setSelection(0);
+                    }
+                    if (animal.equals("bird") && !animal.isEmpty()) {
+                        mypetSpinnerSet.setSelection(3);
+                    }
+                    if (animal.equals("fish") && !animal.isEmpty()) {
+                        mypetSpinnerSet.setSelection(4);
+                    }
                 }
             }
 
