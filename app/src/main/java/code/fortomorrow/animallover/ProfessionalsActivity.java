8bddc22f448f955b01242eb1624da543b22147b8
Kayuemkhan <abdulkayuem007@gmail.com>
2021-03-05 15:48:04 +0600

    bottom sheet updated UI

--- app/src/main/java/code/fortomorrow/animallover/ProfessionalsActivity.java
+++ app/src/main/java/code/fortomorrow/animallover/ProfessionalsActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +22,9 @@ import code.fortomorrow.animallover.fragments.DoctordetailsBottomsheetFragment;
 public class ProfessionalsActivity extends AppCompatActivity {
     private RecyclerView professionalsrecylerview;
     private List<ProfessionalData> exampleList;
+    private List<ProfessionalData> data;
     private ImageView backImageViewIdDoctor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,26 +33,29 @@ public class ProfessionalsActivity extends AppCompatActivity {
         backImageViewIdDoctor = findViewById(R.id.backImageViewIdDoctor);
         professionalsrecylerview.setLayoutManager(new LinearLayoutManager(ProfessionalsActivity.this));
         exampleList = new ArrayList<>();
-        exampleList.add(new ProfessionalData (1, "Dr. Mohammad Mamunur Rashid", "","01725-808534","Central Veterinary Hospital","Veterinary Surgeon","DVM, MS in Theriogenology"));
-        exampleList.add(new ProfessionalData (1, "Dr. Arifur Rabbi", "veterinarybd@hotmail.com","01725-808534","Ta 173/2 Lake view road, Godara ghat to Hatirjheel Road, Dhaka 1212","Veterinary Doctor","DVM, MS (Bangladesh Agricultural University, Mymensingh)"));
-        exampleList.add(new ProfessionalData (1, "Vet Dr. Sagir Uddin Ahmed", "bdvet.com@gmail.com","01912251312","Dr. Sagir's Pet Clinic & Research Center","Pet Doctor, Veterinary Surgeon","DVM, MS in Theriogenology"));
-        exampleList.add(new ProfessionalData (1, "Dr. Md. Mahbubul Alam Bhuiyan", "","01711-146012","67/2, North Bashaboo, Khilgaon, Dhaka","Additional Veterinary officer","Specialist in Treatment, Vaccination & Management of Pet Animal & Birds"));
+        data = new ArrayList<>();
+        exampleList.add(new ProfessionalData(1, "Dr. Mohammad Mamunur Rashid", "", "01725-808534", "Central Veterinary Hospital", "Veterinary Surgeon", "DVM, MS in Theriogenology"));
+        exampleList.add(new ProfessionalData(1, "Dr. Arifur Rabbi", "veterinarybd@hotmail.com", "01725-808534", "Ta 173/2 Lake view road, Godara ghat to Hatirjheel Road, Dhaka 1212", "Veterinary Doctor", "DVM, MS (Bangladesh Agricultural University, Mymensingh)"));
+        exampleList.add(new ProfessionalData(1, "Vet Dr. Sagir Uddin Ahmed", "bdvet.com@gmail.com", "01912251312", "Dr. Sagir's Pet Clinic & Research Center", "Pet Doctor, Veterinary Surgeon", "DVM, MS in Theriogenology"));
+        exampleList.add(new ProfessionalData(1, "Dr. Md. Mahbubul Alam Bhuiyan", "", "01711-146012", "67/2, North Bashaboo, Khilgaon, Dhaka", "Additional Veterinary officer", "Specialist in Treatment, Vaccination & Management of Pet Animal & Birds"));
 
-        professionalsrecylerview.setAdapter(new ProfessionalsAdapters(ProfessionalsActivity.this,exampleList));
+        professionalsrecylerview.setAdapter(new ProfessionalsAdapters(ProfessionalsActivity.this, exampleList));
         backImageViewIdDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ProfessionalsActivity.this,HomeActivity.class));
+                startActivity(new Intent(ProfessionalsActivity.this, HomeActivity.class));
             }
         });
     }
 
     public void showDoctorDetails(String professionalName) {
-        Log.d("professionalNameMain",""+professionalName);
         DoctordetailsBottomsheetFragment addPhotoBottomDialogFragment =
-                new DoctordetailsBottomsheetFragment();
-        Bundle bundle = new  Bundle();
-        bundle.putString("key", professionalName);
+                new DoctordetailsBottomsheetFragment(this);
+        Bundle bundle = new Bundle();
+        String json = new Gson().toJson(exampleList);
+        bundle.putString("key", json);
+        bundle.putString("professionalName",professionalName);
+
         addPhotoBottomDialogFragment.setArguments(bundle);
         addPhotoBottomDialogFragment.show(getSupportFragmentManager(),
                 "DoctordetailsBottomsheetFragment");
@@ -58,8 +65,4 @@ public class ProfessionalsActivity extends AppCompatActivity {
 //        }
     }
 
-    private void setData(String professionalName) {
-        DoctordetailsBottomsheetFragment fragmentDemo = (DoctordetailsBottomsheetFragment) getSupportFragmentManager().findFragmentByTag("DoctordetailsBottomsheetFragment");
-        fragmentDemo.setListData(exampleList,professionalName);
-    }
 }
--- app/src/main/java/code/fortomorrow/animallover/fragments/DoctordetailsBottomsheetFragment.java
+++ app/src/main/java/code/fortomorrow/animallover/fragments/DoctordetailsBottomsheetFragment.java
@@ -1,9 +1,11 @@
 package code.fortomorrow.animallover.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,39 +13,93 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import code.fortomorrow.animallover.ModelClass.ProfessionalData;
+import code.fortomorrow.animallover.ProfessionalsActivity;
 import code.fortomorrow.animallover.R;
+import code.fortomorrow.animallover.utils.SharedPref;
 
 public class DoctordetailsBottomsheetFragment extends BottomSheetDialogFragment {
     private List<ProfessionalData> professionalData;
-    private TextView doctorname;
+    private TextView doctorname,email,chamber,desination,qualification,cell;
+    private TextView callme,emailme;
     String data;
-    public DoctordetailsBottomsheetFragment() {
-
+    String professionalName;
+    ProfessionalData[] langs;
+    ProfessionalsActivity professionalsActivity;
+    public DoctordetailsBottomsheetFragment(ProfessionalsActivity professionalsActivity) {
+        this.professionalsActivity = professionalsActivity;
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        SharedPref.init(getActivity());
         setStyle(DialogFragment.STYLE_NO_FRAME,R.style.AppBottomSheetDialogTheme);
     }
-    public static final String TAG = "DoctordetailsBottomsheetFragment";
-    public static DoctordetailsBottomsheetFragment newInstance() {
-        return new DoctordetailsBottomsheetFragment();
-    }
+   // public static final String TAG = "DoctordetailsBottomsheetFragment";
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_doctordetails_bottomsheet, container, false);
-        data =  getArguments().getString("key");
-        Log.d("key",""+data);
         doctorname =view.findViewById(R.id.doctorname);
-        doctorname.setText(String.valueOf(data));
+        email =view.findViewById(R.id.email);
+        chamber =view.findViewById(R.id.chamber);
+        desination =view.findViewById(R.id.desination);
+        qualification =view.findViewById(R.id.qualification);
+        cell =view.findViewById(R.id.cell);
+        callme =view.findViewById(R.id.callme);
+        callme.isClickable();
+        emailme =view.findViewById(R.id.emailme);
+        callme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(SharedPref.read("doctorPhone","")));
+                startActivity(intent);
+                dismiss();
+            }
+        });
+
+
+        data =  getArguments().getString("key");
+        professionalName = getArguments().getString("professionalName");
+        Log.d("professionalName",""+professionalName);
+        final ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            langs = objectMapper.readValue(data, ProfessionalData[].class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        List<ProfessionalData> langList = new ArrayList(Arrays.asList(langs));
+
+
+        for(int i =0;i<langList.size();i++){
+            if(langList.get(i).getProfessionalName().contains(professionalName)){
+                doctorname.setText(langList.get(i).getProfessionalName());
+                if(!langList.get(i).getEmail().isEmpty()){
+                    SharedPref.write("doctorEmail",langList.get(i).getEmail());
+                    email.setText(langList.get(i).getEmail());
+                }
+                else {
+                    email.setText("No Email Is Available");
+                }
+                chamber.setText(langList.get(i).getChamber());
+                desination.setText(langList.get(i).getDesination());
+                qualification.setText(langList.get(i).getQualification());
+                SharedPref.write("doctorPhone",langList.get(i).getCell());
+                cell.setText("Cell : "+langList.get(i).getCell());
+            }
+
+        }
         return view;
     }
 
@@ -51,10 +107,6 @@ public class DoctordetailsBottomsheetFragment extends BottomSheetDialogFragment 
     public int getTheme() {
         return R.style.AppBottomSheetDialogTheme;
     }
-    public void setListData(List<ProfessionalData> exampleList, String professionalName){
 
-        Log.d("professionalName",""+professionalName);
-        professionalData = exampleList;
-    }
 
 }
